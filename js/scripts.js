5600fd87ac35a906edd4437bcaa116d28b66a616
Anastasia Rusakova <runas123@ya.ru>
2015-01-22 14:31:34 +0200

    added slider

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){	
+	var countClick = 0;
+
+	$('.carousel').append('<div class="On"></div>');
+	$('.carousel').click(function(){		
+		if(countClick === 2)
+		{
+			$('.On').animate({left: '-=204px'},500);
+			countClick = 0;
+		}else {
+			$('.On').animate({left: '+=102px'}, 500);
+			countClick++;
+		}
+
+		var $slide_element = $('.head-slider .col-md-3:first-child');
+		//$slide_element.fadeOut('fast', function(){
+		//	$(this).appendTo('.wrap-slider').fadeIn('400')
+		//});
+		$slide_element.hide('3000', function() {
+			$(this).remove();
+			$slide_element.appendTo('.wrap-slider').show('3000');
+		
+		});
+
+    	
+
+	});
+			
+});
